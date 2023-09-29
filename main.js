49712d212128c51f362ab55b54142b40288fcde9
YumaMorimoto <kanten520212@gmail.com>
2023-09-30 02:26:56 +0900

    :art: コードフォーマット実施

--- main.js
+++ main.js
@@ -1,71 +1,70 @@
 const countdown = () => {
-    const now = new Date();
-    const target = new Date("2023/11/5 18:00:00");
-    const differ = target.getTime() - now.getTime();
-  
-    if (differ < 0) return false;
-  
-    const ms = Math.floor(differ);
-    const sec = Math.floor((differ / 1000) % 60);
-    const min = Math.floor((differ / 1000 / 60) % 60);
-    const hour = Math.floor((differ / 1000 / 60 / 60) % 24);
-    const day = Math.floor(differ / 1000 / 60 / 60 / 24);
-  
-    document.querySelector(".js-countdown-ms").textContent = String(ms).slice(
-      -3,
-      -1
-    );
-    document.querySelector(".js-countdown-sec").textContent = String(
-      sec
-    ).padStart(2, "0");
-    document.querySelector(".js-countdown-min").textContent = String(
-      min
-    ).padStart(2, "0");
-    document.querySelector(".js-countdown-hour").textContent = String(
-      hour
-    ).padStart(2, "0");
-    document.querySelector(".js-countdown-day").textContent = String(
-      day
-    ).padStart(2, "0");
-  
-    if (differ < 0) clearInterval(countdown);
-  };
-  setInterval(countdown, 1);
-  
-  const video = document.querySelector("#video");
-  const videoSrc = "images/movie.m3u8";
-  
-  if (Hls.isSupported()) {
-    const hls = new Hls();
-    hls.loadSource(videoSrc);
-    hls.attachMedia(video);
-  } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-    // ネイティブサポートブラウザ用
-    video.src = videoSrc;
-  }
-  
-  // スライド
-  let vw = 0.04266666667 * window.innerWidth;
-  var mySwiper = new Swiper(".swiper-container", {
-    centeredSlides: true,
-    loop: true,
-    autoHeight: true,
-  
-    slidesPerView: "auto",
-    spaceBetween: 10,
-    centeredSlides: true,
-    breakpoints: {
-      767: {
-        spaceBetween: vw,
-        slidesPerView: 1,
-        slidesPerView: "auto",
-      },
+  const now = new Date();
+  const target = new Date('2023/11/5 18:00:00');
+  const differ = target.getTime() - now.getTime();
+
+  if (differ < 0) return false;
+
+  const ms = Math.floor(differ);
+  const sec = Math.floor((differ / 1000) % 60);
+  const min = Math.floor((differ / 1000 / 60) % 60);
+  const hour = Math.floor((differ / 1000 / 60 / 60) % 24);
+  const day = Math.floor(differ / 1000 / 60 / 60 / 24);
+
+  document.querySelector('.js-countdown-ms').textContent = String(ms).slice(
+    -3,
+    -1
+  );
+  document.querySelector('.js-countdown-sec').textContent = String(
+    sec
+  ).padStart(2, '0');
+  document.querySelector('.js-countdown-min').textContent = String(
+    min
+  ).padStart(2, '0');
+  document.querySelector('.js-countdown-hour').textContent = String(
+    hour
+  ).padStart(2, '0');
+  document.querySelector('.js-countdown-day').textContent = String(
+    day
+  ).padStart(2, '0');
+
+  if (differ < 0) clearInterval(countdown);
+};
+setInterval(countdown, 1);
+
+const video = document.querySelector('#video');
+const videoSrc = 'images/movie.m3u8';
+
+if (Hls.isSupported()) {
+  const hls = new Hls();
+  hls.loadSource(videoSrc);
+  hls.attachMedia(video);
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+  // ネイティブサポートブラウザ用
+  video.src = videoSrc;
+}
+
+// スライド
+let vw = 0.04266666667 * window.innerWidth;
+var mySwiper = new Swiper('.swiper-container', {
+  centeredSlides: true,
+  loop: true,
+  autoHeight: true,
+
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  centeredSlides: true,
+  breakpoints: {
+    767: {
+      spaceBetween: vw,
+      slidesPerView: 1,
+      slidesPerView: 'auto',
     },
-    autoplay: {
-      delay: 3000,
-      stopOnLastSlide: false,
-      disableOnInteraction: false,
-      reverseDirection: false,
-    },
-  });
-  
+  },
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+    reverseDirection: false,
+  },
+});
--- smoke-1.js
+++ smoke-1.js
@@ -1,98 +1,107 @@
 window.addEventListener('DOMContentLoaded', initSmoke1);
 
 function initSmoke1() {
-    var camera, scene, renderer,
-        geometry, material, mesh;
+  var camera, scene, renderer, geometry, material, mesh;
 
-    var elm = document.getElementById("smoke-1");
+  var elm = document.getElementById('smoke-1');
 
-    init1();
-    animate1();
+  init1();
+  animate1();
 
-    function init1() {
-        clock1 = new THREE.Clock();
+  function init1() {
+    clock1 = new THREE.Clock();
 
-        renderer = new THREE.WebGLRenderer({
-            canvas: document.querySelector('#myCanvas-1')
-        });
-        renderer.setSize(elm.clientWidth, elm.clientHeight);
-
-        scene = new THREE.Scene();
-
-        camera = new THREE.PerspectiveCamera(90, elm.clientWidth / elm.clientHeight, 1, 10000);
-        camera.position.z = 1000;
-        scene.add(camera);
-
-        geometry = new THREE.BoxGeometry(200, 200, 200);
-        material = new THREE.MeshLambertMaterial({ color: 0xaa6666, wireframe: false });
-        mesh = new THREE.Mesh(geometry, material);
-        // scene.add( mesh );
-        cubeSineDriver = 0;
-
-        light = new THREE.DirectionalLight(0xffffff, 0xffffff, 1.0);
-        light.position.set(-1, 0, 1);
-        scene.add(light);
-
-
-        smokeTexture = new THREE.TextureLoader().load('images/Smoke-Element.png');
-        smokeMaterial = new THREE.MeshLambertMaterial({ color: 0xE4E4E4, map: smokeTexture, transparent: true, opacity: 0.1 });
-        smokeGeo = new THREE.PlaneGeometry(300, 300);
-        smokeParticles1 = [];
-
-        // p個のparticleメッシュを生成している
-        for (p = 0; p < 70; p++) {
-            var particle = new THREE.Mesh(smokeGeo, smokeMaterial);
-            var radian = Math.random() * 2 * Math.PI;
-            var sinValue = Math.sin(radian);
-            particle.position.set(
-                sinValue * (Math.random() * 400) - 200,
-                sinValue * (Math.random() * 500) + 50,
-                Math.random() * 1000 - 100
-            );
-
-            particle.rotation.z = Math.random() * 360;
-            scene.add(particle);
-            smokeParticles1.push(particle);
-        }
-
-        elm.appendChild(renderer.domElement);
-
-    }
-
-
-    function animate1() {
-        // console.log('animate function is called');
-        delta = clock1.getDelta();
-        requestAnimationFrame(animate1);
-        evolveSmoke1();
-        render1();
-    }
-
-    function evolveSmoke1() {
-        var sp = smokeParticles1.length;
-        while (sp--) {
-            // textureの回転速度を調整
-            smokeParticles1[sp].rotation.z += (delta * 0.165);
-        }
-    }
-
-    function render1() {
-        mesh.rotation.x += 0.005;
-        mesh.rotation.y += 0.01;
-        cubeSineDriver += .01;
-        mesh.position.z = 100 + (Math.sin(cubeSineDriver) * 500);
-        renderer.render(scene, camera);
-    }
-
-    window.addEventListener('resize', function () {
-        const width = elm.clientWidth;
-        const height = elm.clientHeight;
-
-        // レンダラーのサイズを調整する
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(width, height);
-
-        // カメラのアスペクト比を正す
-        camera.updateProjectionMatrix();
+    renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector('#myCanvas-1'),
     });
+    renderer.setSize(elm.clientWidth, elm.clientHeight);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(
+      90,
+      elm.clientWidth / elm.clientHeight,
+      1,
+      10000
+    );
+    camera.position.z = 1000;
+    scene.add(camera);
+
+    geometry = new THREE.BoxGeometry(200, 200, 200);
+    material = new THREE.MeshLambertMaterial({
+      color: 0xaa6666,
+      wireframe: false,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    // scene.add( mesh );
+    cubeSineDriver = 0;
+
+    light = new THREE.DirectionalLight(0xffffff, 0xffffff, 1.0);
+    light.position.set(-1, 0, 1);
+    scene.add(light);
+
+    smokeTexture = new THREE.TextureLoader().load('images/Smoke-Element.png');
+    smokeMaterial = new THREE.MeshLambertMaterial({
+      color: 0xe4e4e4,
+      map: smokeTexture,
+      transparent: true,
+      opacity: 0.1,
+    });
+    smokeGeo = new THREE.PlaneGeometry(300, 300);
+    smokeParticles1 = [];
+
+    // p個のparticleメッシュを生成している
+    for (p = 0; p < 70; p++) {
+      var particle = new THREE.Mesh(smokeGeo, smokeMaterial);
+      var radian = Math.random() * 2 * Math.PI;
+      var sinValue = Math.sin(radian);
+      particle.position.set(
+        sinValue * (Math.random() * 400) - 200,
+        sinValue * (Math.random() * 500) + 50,
+        Math.random() * 1000 - 100
+      );
+
+      particle.rotation.z = Math.random() * 360;
+      scene.add(particle);
+      smokeParticles1.push(particle);
+    }
+
+    elm.appendChild(renderer.domElement);
+  }
+
+  function animate1() {
+    // console.log('animate function is called');
+    delta = clock1.getDelta();
+    requestAnimationFrame(animate1);
+    evolveSmoke1();
+    render1();
+  }
+
+  function evolveSmoke1() {
+    var sp = smokeParticles1.length;
+    while (sp--) {
+      // textureの回転速度を調整
+      smokeParticles1[sp].rotation.z += delta * 0.165;
+    }
+  }
+
+  function render1() {
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.01;
+    cubeSineDriver += 0.01;
+    mesh.position.z = 100 + Math.sin(cubeSineDriver) * 500;
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener('resize', function () {
+    const width = elm.clientWidth;
+    const height = elm.clientHeight;
+
+    // レンダラーのサイズを調整する
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    // カメラのアスペクト比を正す
+    camera.updateProjectionMatrix();
+  });
 }
