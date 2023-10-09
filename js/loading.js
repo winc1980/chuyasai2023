698e2db2fea48e60be84a18c5f46fff38b75f252
YumaMorimoto <kanten520212@gmail.com>
2023-10-09 16:08:20 +0900

    formatting

--- js/loading.js
+++ js/loading.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded.");
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded.');
 
   var tl1 = gsap.timeline();
   tl1
-    .to("#loadingLogo", {
+    .to('#loadingLogo', {
       scale: 1,
-      ease: "back.out(0.8)",
+      ease: 'back.out(0.8)',
       duration: 1,
     })
     .pause();
 
   var tl2 = gsap.timeline();
   tl2
-    .to("#loadingLogo", { rotation: 1440, duration: 1 })
-    .to("#loadingLogo", {
+    .to('#loadingLogo', { rotation: 1440, duration: 1 })
+    .to('#loadingLogo', {
       scale: 1.1,
-      ease: "elastic.out(10, 0.3)",
+      ease: 'elastic.out(10, 0.3)',
       duration: 1,
     })
     .repeat(-1)
     .pause();
 
-  gsap.set("#loadingLogo", { scale: 30, opacity: 1 });
+  gsap.set('#loadingLogo', { scale: 30, opacity: 1 });
 
   tl1.play();
   tl1.then(() => {
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 window.onload = () => {
   setTimeout(() => {
-    document.querySelector(".loading").setAttribute("class", "loading d-none");
+    document.querySelector('.loading').setAttribute('class', 'loading d-none');
   }, 2000);
-  console.log("onload.");
+  console.log('onload.');
 };
