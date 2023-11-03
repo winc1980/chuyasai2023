f6642d53c5001d75b739806e7ca44336d2dfca38
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2023-11-03 14:25:45 +0900

    convert html to php

--- home.php
+++ home.php
@@ -38,81 +38,71 @@
       </div>
     </div>
   </section>
-
-  <section id="explain">
-    <img class="line" src="<?php echo get_theme_file_uri(
-      'images/topline.webp'
-    ); ?>" alt="" />
-    <div class="explain-content">
-      <p>
-        <span>誰</span>が<span>いつ</span>始めたのかわからない。<br />決まり事は<br /><span>「4年生が行う」</span><br /><span>「1日目の隈ステの大トリ」</span><br /><span>ただこれだけである。</span><br />学園祭最大規模の観客4000人を動員する名物企画<br />陽が沈み煌びやかなステージで、<br />現役の早稲田に所属する多くの演者と協力団体で<br />早稲田祭を盛り上げる！！
-      </p>
-      <div class="black-image">
-        <h1>中夜祭とは</h1>
+  <div id="smoke-1">
+        <div class="smoke-wrap-1">
+          <section id="explain">
+            <img class="line" src="<?php echo get_theme_file_uri("images/topline.webp"); ?>" alt="" />
+            <div class="explain-content">
+              <p>
+                <span>誰</span>が<span>いつ</span>始めたのかわからない。<br />決まり事は<br /><span>「4年生が行う」</span><br /><span>「1日目の隈ステの大トリ」</span><br /><span>ただこれだけである。</span><br />学園祭最大規模の観客4000人を動員する名物企画<br />陽が沈み煌びやかなステージで、<br />現役の早稲田に所属する多くの演者と協力団体で<br />早稲田祭を盛り上げる！！
+              </p>
+              <div class="black-image">
+                <h1>中夜祭とは</h1>
+              </div>
+            </div>
+          </section>
+          <div class="swiper-parent fade">
+            <!-- Swiper START -->
+            <div class="swiper-container">
+              <!-- メイン表示部分 -->
+              <div class="swiper-wrapper">
+                <!-- 各スライド -->
+                <div class="swiper-slide"><img src="<?php echo get_theme_file_uri("images/2022_1.webp"); ?>" alt="Swiper01"></div>
+                <div class="swiper-slide"><img src="<?php echo get_theme_file_uri("images/2022_2.webp"); ?>" alt="Swiper02"></div>
+                <div class="swiper-slide"><img src="<?php echo get_theme_file_uri("images/girls.JPG"); ?>" alt="Swiper03"></div>
+              </div>
+              <img class="line" src="<?php echo get_theme_file_uri("images/bottomline.webp"); ?>" alt="" />
+            </div>
+          </div>
+        </div>
+        <canvas id="myCanvas-1"></canvas>
       </div>
-    </div>
-  </section>
-  <div class="swiper-parent fade">
-    <!-- Swiper START -->
-    <div class="swiper-container">
-      <!-- メイン表示部分 -->
-      <div class="swiper-wrapper">
-        <!-- 各スライド -->
-        <div class="swiper-slide"><img src="<?php echo get_theme_file_uri(
-          'images/2022_1.webp'
-        ); ?>" alt="Swiper01"></div>
-        <div class="swiper-slide"><img src="<?php echo get_theme_file_uri(
-          'images/2022_2.webp'
-        ); ?>" alt="Swiper02"></div>
-        <div class="swiper-slide"><img src="<?php echo get_theme_file_uri(
-          'images/girls.JPG'
-        ); ?>" alt="Swiper03"></div>
-    </div>
-    <img class="line" src="<?php echo get_theme_file_uri(
-      'images/bottomline.webp'
-    ); ?>" alt="" />
-  </div>
-  <div class="overview">
-    <h1>企画概要</h1>
-    <div class="story">
-      <img class="storyimg" src="<?php echo get_theme_file_uri(
-        'images/Story.webp'
-      ); ?>" alt="">
-      <a href="./about"><img class="storymore" src="<?php echo get_theme_file_uri(
-        'images/arrow.webp'
-      ); ?>" alt=""></a>
-    </div>
-    <div class="performer">
-      <img class="performerimg" src="<?php echo get_theme_file_uri(
-        'images/performer.webp'
-      ); ?>" alt="">
-      <a href="./about#performer"><img class="performermore" src="<?php echo get_theme_file_uri(
-        'images/arrow.webp'
-      ); ?>" alt=""></a>
-    </div>
-  </div>
-  <div class="collaboration">
-    <div class="menubox">
-      <h1>コラボメニュー</h1>
-    </div>
-  </div>
-  <div class="sponsor">
-    <img class="rainbow-vertical" src="<?php echo get_theme_file_uri(
-      'images/縦１.webp'
-    ); ?>" alt="" />
-    <div class="sponsor-title">
-      <img src="<?php echo get_theme_file_uri(
-        'images/sponsor.webp'
-      ); ?>" alt="" />
-      <div class="sponsor-decoration"></div>
-    </div>
-    <img class="rainbow-horizontal" src="<?php echo get_theme_file_uri(
-      'images/横２.webp'
-    ); ?>" alt="" />
-    <a href=""><img class="more" src="<?php echo get_theme_file_uri(
-      'images/arrow.webp'
-    ); ?>" alt="" /></a>
-  </div>
+      <div id="smoke-2">
+        <div class="smoke-wrap-2">
+          <canvas id="myCanvas-2"></canvas>
+          <div class="overview ">
+            <h1>企画概要</h1>
+            <div class="story">
+              <img class="storyimg" src="<?php echo get_theme_file_uri("images/Story.webp"); ?>" alt="">
+              <a href="about.html"><img class="storymore" src="<?php echo get_theme_file_uri("images/arrow.webp"); ?>" alt=""></a>
+            </div>
+            <div class="performer">
+              <img class="performerimg" src="<?php echo get_theme_file_uri("images/performer.webp"); ?>" alt="">
+              <a href="about.html#performer"><img class="performermore" src="<?php echo get_theme_file_uri("images/arrow.webp"); ?>" alt=""></a>
+            </div>
+          </div>
+
+
+          <div class="collaboration">
+            <a href="sponsors.html"><img src="<?php echo get_theme_file_uri("images/collab_menu_banner.webp"); ?>" alt=""></a>
+          </div>
+        </div>
+        <canvas id="myCanvas-2"></canvas>
+      </div>
+      <div id="smoke-3">
+        <div class="smoke-wrap-3">
+          <div class="sponsor">
+            <img class="rainbow-vertical" src="<?php echo get_theme_file_uri("images/縦１.webp"); ?>" alt="" />
+            <div class="sponsor-title">
+              <img src="<?php echo get_theme_file_uri("images/sponsor.webp"); ?>" alt="" />
+              <div class="sponsor-decoration"></div>
+            </div>
+            <img class="rainbow-horizontal" src="<?php echo get_theme_file_uri("images/横２.webp"); ?>" alt="" />
+            <a href=""><img class="more" src="<?php echo get_theme_file_uri("images/arrow.webp"); ?>" alt="" /></a>
+          </div>
+        </div>
+        <canvas id="myCanvas-3"></canvas>
+      </div>
   <div class="contact">
     <img class="contact-title" src="<?php echo get_theme_file_uri(
       'images/contact.webp'
